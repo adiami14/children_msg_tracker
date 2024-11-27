cd641f3aade49b010d70da7fc06756ab93348f94
adiami <adiamilany@gmail.com>
2024-11-27 06:33:58 +0200

    updates

---
+++ setup/update_hosts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "192.168.2.10       child.child_tracker" >> /etc/hosts
+echo "192.168.2.7       handler.child_tracker" >> /etc/hosts
+echo "192.168.2.9        mother.child_tracker" >> /etc/hosts
